fa7c20226e5723c25f6999ec472b5b02b287663d
Raditya Perdhevi <raditya@perdhevi.com>
2020-05-19 22:30:26 +0700

    FIX: move IAM perm to functions and change name

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '2uknzq4kx7'
+const apiId = 'hr0d0pjrid'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
